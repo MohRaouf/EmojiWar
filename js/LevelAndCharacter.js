64737439511c6aaec1719a5e5b264a0a15c2490b
May Esmail <34819918+MayEsmail@users.noreply.github.com>
2021-01-16 10:54:16 +0200

    Level and Character v2

--- js/LevelAndCharacter.js
+++ js/LevelAndCharacter.js
@@ -5,6 +5,7 @@ var emojis_section=document.getElementsByClassName("available_Emojis")[0];
 var maps_section=document.getElementsByClassName("levels")[0];
 var emoChoises=document.getElementsByClassName("emo");
 var mapChoises=document.getElementsByClassName("lev");
+var msg=document.getElementsByClassName("msg");
 var nick_name=document.getElementById("nickname");
 var locked=[false,true,true];
 var selected_emoji=-1;
@@ -56,18 +57,57 @@ function change_selected_map(index){
     }
 }
 function choose_map(){
+    msg[0].style.display="none";
+    msg[1].style.display="block";
     select_map_btn.style.display="none";
     play_btn.style.display="inline-block";
     change_character.style.display="inline-block";
     emojis_section.style.display="none";
     maps_section.style.display="block";
+    msg[1].innerHTML = msg[1].textContent.replace(/\S/g,"<span class='letter'>$&</span>");
+    anime.timeline({loop: false})
+        .add({
+        targets: msg[1],
+        scale: [4,1],
+        opacity: [0,1],
+        translateZ: 0,
+        easing: "easeOutExpo",
+        duration: 5000,
+        delay: 10
+        }).add({
+        targets: msg[1],
+        duration: 1000,
+        easing: "easeOutExpo",
+        delay: 1000
+        });
+    msg[1].innerHTML='<label>S</label>elect <label>T</label>he <label>M</label>ap <label>O</label>F <label>T</label>he <label>W</label>ar';
 }
 function choose_character(){
+    msg[1].style.display="none";
+    msg[0].style.display="block";
     select_map_btn.style.display="block";
     play_btn.style.display="none";
     change_character.style.display="none";
     emojis_section.style.display="block";
     maps_section.style.display="none";
+    msg[0].innerHTML = msg[0].textContent.replace(/\S/g,"<span class='letter'>$&</span>");
+    anime.timeline({loop: false})
+        .add({
+        targets: msg[0],
+        scale: [4,1],
+        opacity: [0,1],
+        translateZ: 0,
+        easing: "easeOutExpo",
+        duration: 5000,
+        delay: 10
+        }).add({
+        targets: msg[0],
+        duration: 1000,
+        easing: "easeOutExpo",
+        delay: 1000
+        });
+    msg[0].innerHTML='<label>C</label>hoose <label>E</label>moji <label>t</label>o <label>E</label>nter <label>t</label>he <label>w</label>ar';
+
 }
 function play(e){
     if(nick_name.value=="")
