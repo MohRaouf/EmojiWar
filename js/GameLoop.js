f4eaa2b30e5eee93ef050a75360e99c2121643e2
MohRaouf <moh.proff@gmail.com>
2021-01-17 23:36:33 +0200

    Add enemy hit and enemy die sound

--- js/GameLoop.js
+++ js/GameLoop.js
@@ -23,6 +23,7 @@ var enemyArray = [new Enemy(getRandomInt(0, 3), gameScreen)]; // Create array of
 var projectiles = [];
 //Instance of InputHander to Handle the Key strokes
 var inputHandler = new InputHandler(canvas, player);
+var particles = [];
 
 //First Draw of the Player
 player.draw(context, inputHandler.mouse);
@@ -45,7 +46,7 @@ function gameLoop(timeStamp) {
 
     for (let i = 0; i < projectiles.length; i++) {
         projectiles[i].draw(context, deltaTime);
-        if (projectiles[i].isHit(enemyArray, player)) {
+        if (projectiles[i].isHit(enemyArray, player,context,particles)) {
             projectiles.splice(i, 1);
         }
     }
@@ -53,6 +54,17 @@ function gameLoop(timeStamp) {
     player.move(inputHandler.held_directions, deltaTime); //Move the player to the held directions
     player.draw(context, inputHandler.mouse);             //Then Draw the Player
 
+    // particles.forEach((particle, index) => {
+    //     //[7]particles fadeout
+    //     if (particle.alpha <= 0) {
+
+    //         particles.splice(index, 1)
+    //     } else {
+    //         particle.update();
+    //     }
+    // })
+    //[7]explosion of enemy
+ 
 
     //detect if the player is shooting and if so fire a projectile and the effects
     player.shoot(inputHandler.isShooting);
