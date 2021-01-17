7c65ca4260eb0700c8b786157076c9593427eee7
Ibrahim Mohamed <76173581+i-salama@users.noreply.github.com>
2021-01-17 23:33:27 +0200

    Congratulation modification

---
+++ js/Congratulation.js
@@ -0,0 +1,42 @@
+const winnerMsg = document.querySelector(".winnerMsg")
+const bronze = document.querySelector(".bronze");
+const silver = document.querySelector(".silver");
+const gold = document.querySelector(".gold");
+const platinum = document.querySelector(".platinum");
+const looser = document.querySelector(".looser");
+const nextlevelBtn = document.querySelector(".nextlevelBtn");
+
+function containerResult(score) {
+    if (score == 0) {
+        silver.style.display = 'none';
+        gold.style.display = 'none';
+        platinum.style.display = 'none';
+        bronze.style.display = 'block';
+        confetti.start();
+    } else if (score == 1) {
+        bronze.style.display = 'none';
+        gold.style.display = 'none';
+        platinum.style.display = 'none';
+        silver.style.display = 'block';
+        confetti.start();
+    } else if (score == 2) {
+        bronze.style.display = 'none';
+        silver.style.display = 'none';
+        platinum.style.display = 'none';
+        gold.style.display = 'block';
+        confetti.start();
+    } else if (score == 3) {
+        bronze.style.display = 'none';
+        silver.style.display = 'none';
+        gold.style.display = 'none';
+        platinum.style.display = 'block';
+        confetti.start();
+    } else if (score < 0) {
+        winnerMsg.style.display = 'none';
+        bronze.style.display = 'none';
+        platinum.style.display = 'none';
+        nextlevelBtn.style.display = 'none';
+        looser.style.display = 'block';
+
+    }
+}
